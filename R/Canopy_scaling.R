85055a4a9ae644014b37156c3696b794ce7344bf
Julien Lamour <jlamour@bnl.gov>
2020-11-05 10:28:25 -0500

    Adding a leaf energy budget in the calculation of GPP. The new function is called f.GPPT similarly with f.AT

--- R/Canopy_scaling.R
+++ R/Canopy_scaling.R
@@ -97,8 +97,8 @@ f.GPP<-function(TBM,meteo_hourly,Vcmax_Profile,Jmax_Profile,Rd_Profile,Tp_Profil
   
   Conductance_rate=(gs_dir*canopy$f_sun+gs_dif*(1-canopy$f_sun))
   Trans=(gs_dir*VpdL_dir/Patm*canopy$f_sun+gs_dif*VpdL_dif/Patm*(1-canopy$f_sun))
-  figure_conductance_dir=melt(Conductance_rate)
-  b=(ggplot(data=figure_conductance_dir,aes(x=time,y=Layer,fill=value))+geom_raster()
+  figure_conductance=melt(Conductance_rate)
+  b=(ggplot(data=figure_conductance,aes(x=time,y=Layer,fill=value))+geom_raster()
      +scale_fill_distiller(palette = "Spectral", direction = -1) +scale_y_reverse()
      +xlab("Time in the day")
      +ylab("Vertical level (0= top, 50 = ground)")
@@ -111,6 +111,132 @@ f.GPP<-function(TBM,meteo_hourly,Vcmax_Profile,Jmax_Profile,Rd_Profile,Tp_Profil
   print(paste("ET = ",totalET,"L H20 m-2 Ground Y-1"))
   return(list(A=Photosynthesis_rate,gs=Conductance_rate,A_dir=Photosynthesis_rate_dir,gs_dir=gs_dir,A_dif=Photosynthesis_rate_dif,gs_dif=gs_dif,GPP=totalGPP,ET=totalET,fig_A=a,fig_gs=b))
 }
+
+#' @title Canopy scale GPP calculation, with leaf energy budget
+#' @description Generic function to calculate the GPP within a forest (Here GPP = sum of Anet at the canopy level, so it takes into account the leaf mitochondrial respiration)
+#' @param TBM Specific TBM to use (ORCHIDEE, CLM4.5, FATES or JULES)
+#' @param meteo_hourly Hourly weather data frame with at least the column at (air temperature in degree C) rh (humidity in pc) and sr the total PAR in micro mol m-2 s-1
+#' @param Vcmax_Profile Vector of the values of Vcmax at the reference temperature at each layer of the canopy
+#' @param Jmax_Profile Vector of the values of Jmax at the reference temperature at each layer of the canopy
+#' @param Rd_Profile Vector of the values of Rd at the reference temperature at each layer of the canopy
+#' @param Tp_Profile Vector of the values of Tp at the reference temperature at each layer of the canopy
+#' @param g0_Profile Vector of the values of g0 at the reference temperature at each layer of the canopy
+#' @param g1_Profile Vector of the values of g1 at the reference temperature at each layer of the canopy
+#' @param gsmin Minimum stomatal conductance for water to consider. This value will be used as the minimum conductance value to avoid 0 and negative values obtained from the coupled assimilation and conductance models
+#' @param canopy Description of the canopy interception (see canopy_interception function)
+#' @param Patm Atmospheric pressure (used to calculate the transpiration)
+#' @param ... Other parameters of the photosynthetic model, without gradients, for example curvature factor, quantum yield.. see the help of f.make.param()
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#'## Simulation of photosynthetic gradients
+#' LAI=seq(0,6.2,6.2/49)
+#' Vcmax=f.VcmaxRef.LAI(kn=0.11,LAI=LAI,Vcmax0=70)
+#' Jmax=1.7*Vcmax; Tp=1/5*Vcmax; Rd=0.03*Vcmax
+#' ##Simulation of weather data
+#' meteo_hourly=data.frame(time=0:23,rh=80,at=25,sr=sin(seq(0,pi,pi/23))*2000,tl=25,wind=2)
+#' meteo_hourly[!meteo_hourly$time%in%7:17,'sr']=0
+#' ##Representation of the light interception inside the canopy
+#' canopy=f.canopy.interception(meteo_hourly=meteo_hourly,lat = 9.2801048,t.d = 0:23,DOY = 60,n_layers = 50,Height = 26,LAI = 6)
+#' GPP_sc1=f.GPPT(TBM = "FATES",meteo_hourly = meteo_hourly,Vcmax_Profile = Vcmax,
+#' Jmax_Profile =Jmax ,Rd_Profile =Rd ,Tp_Profile = Tp,
+#' g0_Profile = rep(0.02,length(Vcmax)),g1_Profile = rep(4,length(Vcmax)),canopy=canopy,gsmin = 0.01)
+f.GPPT<-function(TBM,meteo_hourly,Vcmax_Profile,Jmax_Profile,Rd_Profile,Tp_Profile,g0_Profile,g1_Profile,gsmin,canopy,Patm=100,...){
+  VpdL_dir=VpdL_dif=Photosynthesis_rate_dir=Photosynthesis_rate_dif=gs_dir=gs_dif=Tleaf_dir=Tleaf_dif=canopy$canopy_time_dir
+  for(Layer in 1:nrow(canopy$canopy_time_dir)){
+    print(paste('Layer',Layer,'of', nrow(canopy$canopy_time_dir),'layers'))
+    res_dir=f.AT(PFD = canopy$canopy_time_dir[Layer,],
+                cs = 400,
+                Tair = meteo_hourly[,"at"]+273.15,
+                wind= meteo_hourly[,'wind'],
+                RH = meteo_hourly[,"rh"],
+                param = f.make.param(TBM=TBM,
+                                     VcmaxRef =Vcmax_Profile[Layer],
+                                     RdRef = Rd_Profile[Layer],
+                                     JmaxRef=Jmax_Profile[Layer],
+                                     TpRef=Tp_Profile[Layer],
+                                     g0=g0_Profile[Layer],
+                                     g1=g1_Profile[Layer],...
+                ))
+    ls.gs=which(res_dir$gs<gsmin)
+    res_dir$gs[ls.gs]=gsmin
+    res_dir$A[ls.gs]=f.AT(PFD = 0,cs = 400,Tair = meteo_hourly[,"at"]+273.15,RH = meteo_hourly[,"rh"],wind=meteo_hourly[,'wind'],param = f.make.param(TBM=TBM,
+                                                                                                                                                             VcmaxRef =Vcmax_Profile[Layer],
+                                                                                                                                                             RdRef = Rd_Profile[Layer],
+                                                                                                                                                             JmaxRef=Jmax_Profile[Layer],
+                                                                                                                                                             TpRef=Tp_Profile[Layer],
+                                                                                                                                                             g0=1,
+                                                                                                                                                             g1=g1_Profile[Layer]
+    ))$A[ls.gs]
+    #((-g0_Profile[Layer])*400*sqrt(f.ds(Tleaf = meteo_hourly[,"tl"]+273.15,Tair = meteo_hourly[,"at"]+273.15,RH = meteo_hourly[,"rh"])/1000)/(1.6*g1_Profile[Layer]))[ls.gs]
+    Photosynthesis_rate_dir[Layer,]=res_dir$A
+    VpdL_dir[Layer,]=res_dir$ds/1000
+    gs_dir[Layer,]=res_dir$gs
+    Tleaf_dir[Layer,]=res_dir$Tleaf
+    res_dif=f.AT(PFD = canopy$canopy_time_dif[Layer,],
+                cs = 400,
+                Tair = meteo_hourly[,"at"]+273.15,
+                wind=meteo_hourly[,'wind'],
+                RH = meteo_hourly[,"rh"],
+                param = f.make.param(TBM=TBM,
+                                     VcmaxRef =Vcmax_Profile[Layer],
+                                     RdRef = Rd_Profile[Layer],
+                                     JmaxRef=Jmax_Profile[Layer],
+                                     TpRef=Tp_Profile[Layer],
+                                     g0=g0_Profile[Layer],
+                                     g1=g1_Profile[Layer],...
+                ))
+    ls.gs=which(res_dif$gs<gsmin)
+    res_dif$gs[ls.gs]=gsmin
+    res_dif$A[ls.gs]=f.AT(PFD = 0,cs = 400,Tair = meteo_hourly[,"at"]+273.15,RH = meteo_hourly[,"rh"],wind=meteo_hourly[,'wind'],param = f.make.param(TBM=TBM,
+                                                                                                                                                             VcmaxRef =Vcmax_Profile[Layer],
+                                                                                                                                                             RdRef = Rd_Profile[Layer],
+                                                                                                                                                             JmaxRef=Jmax_Profile[Layer],
+                                                                                                                                                             TpRef=Tp_Profile[Layer],
+                                                                                                                                                             g0=1,
+                                                                                                                                                             g1=g1_Profile[Layer]
+    ))$A[ls.gs]
+    Photosynthesis_rate_dif[Layer,]=res_dif$A
+    gs_dif[Layer,]=res_dif$gs
+    VpdL_dif[Layer,]=res_dif$ds/1000
+    Tleaf_dif[Layer,]=res_dif$Tleaf
+  }
+  Photosynthesis_rate=(Photosynthesis_rate_dir*canopy$f_sun+Photosynthesis_rate_dif*(1-canopy$f_sun))
+  figure_photosynthesis=melt(Photosynthesis_rate)
+  a=(ggplot(data=figure_photosynthesis,aes(x=time,y=Layer,fill=value))+geom_raster()
+     +scale_fill_distiller(palette = "Spectral", direction = -1) +scale_y_reverse()
+     +xlab("Time in the day")
+     +ylab("Vertical level (0= top, 50 = ground)")
+     +labs(fill=expression(A~(mu~mol~m^-2~s^-1))))
+  
+  Conductance_rate=(gs_dir*canopy$f_sun+gs_dif*(1-canopy$f_sun))
+  Trans=(gs_dir*VpdL_dir/Patm*canopy$f_sun+gs_dif*VpdL_dif/Patm*(1-canopy$f_sun))
+  Tleaf=(Tleaf_dir*canopy$f_sun+Tleaf_dif*(1-canopy$f_sun))
+  figure_conductance=melt(Conductance_rate)
+  b=(ggplot(data=figure_conductance,aes(x=time,y=Layer,fill=value))+geom_raster()
+     +scale_fill_distiller(palette = "Spectral", direction = -1) +scale_y_reverse()
+     +xlab("Time in the day")
+     +ylab("Vertical level (0= top, 50 = ground)")
+     +labs(fill=expression(g[sw]~(mol~m^-2~s^-1))))
+  figure_Tleaf=melt(Tleaf)
+  c=(ggplot(data=figure_Tleaf,aes(x=time,y=Layer,fill=value))+geom_raster()
+     +scale_fill_distiller(palette = "Spectral", direction = -1) +scale_y_reverse()
+     +xlab("Time in the day")
+     +ylab("Vertical level (0= top, 50 = ground)")
+     +labs(fill=expression(Tleaf~(K))))
+  print(a)
+  print(b)
+  print(c)
+  totalGPP=mean(Photosynthesis_rate,na.rm=TRUE)*max(canopy$Light_Profile$LAI)*365*3600*24*44/10^6
+  totalET= mean(Trans,na.rm=TRUE)*max(canopy$Light_Profile$LAI)*365*3600*24*18*10^-3
+  print(paste("GPP = ",totalGPP,"g CO2 m-2 Ground Y-1"))
+  print(paste("ET = ",totalET,"L H20 m-2 Ground Y-1"))
+  return(list(A=Photosynthesis_rate,gs=Conductance_rate,A_dir=Photosynthesis_rate_dir,gs_dir=gs_dir,A_dif=Photosynthesis_rate_dif,gs_dif=gs_dif,Tleaf_dir=Tleaf_dir,Tleaf_dif=Tleaf_dif,Tleaf=Tleaf,GPP=totalGPP,ET=totalET,fig_A=a,fig_gs=b,fig_Tleaf=c))
+}
+
+
 
 #' @title Gradients of photosynthetic parameters
 #' @description Several versions of gradients can be found in the litterature, see for example Lloyd et al. 2010 (Fig. 10 and equation A2), but also the equation A14 from Krinner et al. 2005 and the equation 33 from Clark et al. 2011
